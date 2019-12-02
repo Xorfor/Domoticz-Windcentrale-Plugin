18ccfe063acd3d2329bc843dd301446f37135425
Xorfor <xorfor@hotmail.com>
2019-12-02 20:22:39 +0100

    Changed error message to log

--- plugin.py
+++ plugin.py
@@ -443,9 +443,9 @@ class BasePlugin:
         if Connection.Name == self.CONN_PROD:
             data = ET.fromstring(Data["Data"])
             sum = float(data.find("./productie/subset[@period='DAY']").get("sum"))
-            Domoticz.Error("sum: {}".format(int(sum)))
+            Domoticz.Log("sum: {}".format(int(sum)))
             self.__wnd_kwh = (self.__number_winddelen * sum) / self.__max_winddelen
-            Domoticz.Error("self.__wnd_kwh: {}".format(1000 * self.__wnd_kwh))
+            Domoticz.Log("self.__wnd_kwh: {}".format(1000 * self.__wnd_kwh))
             UpdateDevice(
                 Unit.DKWHWND,
                 0,
